2916fe166a30b7c88c2fa80dd5ba3c773d957f33
progfay <shun1856shun@gmail.com>
2019-03-16 13:45:59 +0900

    feat: support for AND search

--- index.js
+++ index.js
@@ -2,12 +2,24 @@ const alfy = require('alfy')
 const { lib } = require('emojilib')
 const emojisyo = require('./emojisyo')
 
+const keywordList = Object.keys(emojisyo)
+
+const getEmojis = keyword => (
+  alfy.matches(keyword, keywordList)
+    .map(key => emojisyo[key])
+    .flat()
+)
+
+let items = null
+const inputs = alfy.input.split(/\s+/g).filter(Boolean)
+for (const input of inputs) {
+  items = items
+    ? new Set(getEmojis(input).filter(emoji => items.has(emoji)))
+    : new Set(getEmojis(input))
+}
+
 alfy.output(
-  Array.from(new Set(
-    alfy.inputMatches(Object.keys(emojisyo))
-      .map(keyword => emojisyo[keyword])
-      .flat()
-  )).map(name => ({
+  Array.from(items).map(name => ({
     title: `${(lib[name] || {}).char} :${name}:`,
     subtitle: lib[name].keywords.join(', '),
     emoji: lib[name].char,
